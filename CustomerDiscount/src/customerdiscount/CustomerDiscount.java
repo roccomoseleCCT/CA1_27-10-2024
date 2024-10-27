aa76fa14212dd330908cb5cef9736d923590039c
roccomoseleCCT <sba24436@student.cct.ie>
2024-10-27 14:58:54 +0000

    Reading of the file in input
    Reading of the file in input using the BufferReader and FileReader classes and adding of the customer.txt in the folder

--- CustomerDiscount/src/customerdiscount/CustomerDiscount.java
+++ CustomerDiscount/src/customerdiscount/CustomerDiscount.java
@@ -4,17 +4,33 @@
  */
 package customerdiscount;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
- *
- * @author User
+ * @author Rocco Mosele 27/10/2024
  */
 public class CustomerDiscount {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args){
+        // Initialize path input 
+        String inputFile="C:\\Users\\User\\Desktop\\CCT\\CCT Monday\\CA1_27-10-2024\\CustomerDiscount\\src\\customerdiscount\\customer.txt";
+        //Initialize path output
+        String outputFile="C:\\Users\\User\\Desktop\\CCT\\CCT Monday\\CA1_27-10-2024\\CustomerDiscount\\src\\customerdiscount\\customerdiscount.txt";
+        //Reading of the input file using the try because could generate a FileNotFoundException
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
+            
+        } catch(IOException e){
+            //Print the error message in case there are some mistakes with the files
+            e.printStackTrace();
+        }
+        
+        
     }
     
 }
